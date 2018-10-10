a7338fead50babd1f84e300fe5251aaa599d5424
Ihor Khrystiuk <i.khrystiuk@soft-industry.com>
2018-10-10 15:34:57 +0300

    feat: conditional rendering and default props

--- src/components/Price.js
+++ src/components/Price.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Price = ({ cents }) => {
-  return <span className="ui green ribbon label">$ {cents / 100}</span>;
-};
+const Price = ({ cents }) => (
+  <span className="ui green ribbon label">$ {cents / 100}</span>
+);
 
 Price.propTypes = {
   cents: PropTypes.number.isRequired
--- src/containers/GameList.js
+++ src/containers/GameList.js
@@ -5,14 +5,26 @@ import GameCard from 'GameCard';
 
 const GameList = ({ games }) => (
   <div className="ui four cards">
-    {games.map(game => (
-      <GameCard game={game} key={game._id} />
-    ))}
+    {games.length === 0 ? (
+      <div className="ui icon message">
+        <i className="icon info" />
+        <div className="content">
+          <div className="header">There are no game in your store</div>
+          <p>You should add some, don't you think?</p>
+        </div>
+      </div>
+    ) : (
+      games.map(game => <GameCard game={game} key={game._id} />)
+    )}
   </div>
 );
 
 GameList.propTypes = {
   games: PropTypes.arrayOf(PropTypes.object).isRequired
+};
+
+GameList.defaultProps = {
+  games: []
 };
 
 export default GameList;
